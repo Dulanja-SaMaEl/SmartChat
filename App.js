e88b0911ed062d887a61707624ac71e4c5e3c2f3
Dulanja Abeysinghe <dulanja150abeysinghe@gmail.com>
2024-11-22 19:04:09 +0530

    New Chat App

--- App.js
+++ App.js
@@ -1,20 +1,192 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import * as SplashScreen from "expo-splash-screen";
+import { Image } from "expo-image";
+import { useFonts } from "expo-font";
+import { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import * as ImagePicker from "expo-image-picker";
+import {
+  FontAwesome,
+  FontAwesome6,
+  FontAwesome4,
+  FontAwesome5,
+  AntDesign,
+  MaterialIcons,
+} from "@expo/vector-icons";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [getImage, setImage] = useState(null);
+
+  const [loaded, error] = useFonts({
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && error) {
+    return null;
+  }
+
+  const logoPath = require("./assets/logo.png");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#859398", "#283048"]} style={stylesheet.view1}>
+      <ScrollView>
+        <View style={stylesheet.view2}>
+          <Image source={logoPath} style={stylesheet.image1} />
+          <Text style={stylesheet.text1}>Create Account</Text>
+          <Text style={stylesheet.text2}>Hello ! Welcome to Smart Chat</Text>
+          <Pressable
+            style={stylesheet.avatar1}
+            onPress={async () => {
+              let result = await ImagePicker.launchImageLibraryAsync({});
+
+              if (!result.canceled) {
+                setImage(result.assets[0].uri);
+              }
+            }}
+          >
+            <Image
+              source={getImage}
+              style={stylesheet.image2}
+              contentFit="container"
+            />
+          </Pressable>
+          <Text style={stylesheet.text3}>Mobile</Text>
+          <TextInput style={stylesheet.input1} inputMode="tel" maxLength={10} />
+          <Text style={stylesheet.text3}>First Name</Text>
+          <TextInput style={stylesheet.input1} inputMode="text" />
+          <Text style={stylesheet.text3}>Last Name</Text>
+          <TextInput style={stylesheet.input1} inputMode="text" />
+          <Text style={stylesheet.text3}>Password</Text>
+          <TextInput
+            style={stylesheet.input1}
+            secureTextEntry={true}
+            inputMode="text"
+            maxLength={20}
+          />
+          <Pressable
+            style={stylesheet.pressable1}
+            onPress={() => {
+              alert("ok");
+            }}
+          >
+            <Text style={stylesheet.text4}>Sign Up</Text>
+            <MaterialIcons name="account-circle" size={24} color="white" />
+          </Pressable>
+          <Pressable
+            style={stylesheet.pressable2}
+            onPress={() => {
+              alert("ok");
+            }}
+          >
+            <Text style={stylesheet.text4}>Already Registered ? Sign In</Text>
+            <AntDesign name="login" size={24} color="white" />
+          </Pressable>
+        </View>
+      </ScrollView>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const stylesheet = StyleSheet.create({
+  view1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  text1: {
+    fontSize: 32,
+    fontFamily: "Poppins-Bold",
+    color: "white",
+  },
+  text2: {
+    fontSize: 20,
+    fontFamily: "Poppins-Regular",
+    color: "white",
+  },
+  text3: {
+    fontSize: 16,
+    fontFamily: "Poppins-Light",
+    color: "white",
+  },
+  text4: {
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+    color: "white",
+  },
+  input1: {
+    width: "100%",
+    height: 50,
+    fontSize: 15,
+    borderStyle: "solid",
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 15,
+    paddingStart: 15,
+    color: "white",
+  },
+  pressable1: {
+    height: 50,
+    backgroundColor: "#0B2F9F",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+    flexDirection: "row",
+    columnGap: 10,
+  },
+  pressable2: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+    flexDirection: "row",
+    columnGap: 10,
+  },
+  image1: {
+    width: 110,
+    height: 100,
+    alignSelf: "center",
+  },
+  image2: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: "center",
+  },
+  avatar1: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  view2: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    rowGap: 5,
   },
 });
